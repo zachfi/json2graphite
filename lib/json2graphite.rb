a0b7e7890852abe72843b3da9495b6c7c59e8d16
Zach Leslie <zach@puppetlabs.com>
2012-07-31 12:48:00 -0700

    add method to return an object rather than puke STDOUT

--- lib/json2graphite.rb
+++ lib/json2graphite.rb
@@ -14,10 +14,20 @@ module Json2Graphite
     end
   end
 
+  # Output graphite formatted data to STDOUT
   def to_graphite (hash, now=Time.now.to_i)
     walk_the_forrest(hash) {|target, value|
       puts "#{target} #{value} #{now}"
     }
+  end
+
+  # Return an array, each line a formatted graphite target
+  def get_graphite (hash, now=Time.now.to_i)
+    data = []
+    walk_the_forrest(hash) {|target, value|
+      data << "#{target} #{value} #{now}"
+    }
+    data
   end
 
 end
