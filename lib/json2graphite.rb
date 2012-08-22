d107346fef8af1626245816c7e8088af0ac5a94f
Zach Leslie <zach@puppetlabs.com>
2012-08-22 09:45:41 -0700

    raise on bad data

--- lib/json2graphite.rb
+++ lib/json2graphite.rb
@@ -2,6 +2,7 @@
 module Json2Graphite
   module_function
 
+  # DOCUMENT THIS!!!
   def walk_the_forrest (obj=self, path=[], &blk)
     obj.each do |key,value|
       case value
@@ -17,6 +18,7 @@ module Json2Graphite
   # outputs to STDOUT.  Takes optional time argument, defaulting to now.
 
   def to_graphite (hash, time=Time.now.to_i)
+    raise "Hash was not received" unless hash.is_a? Hash
     walk_the_forrest(hash) {|target, value|
       puts "#{target} #{value} #{time}"
     }
@@ -24,6 +26,7 @@ module Json2Graphite
 
   # Return an array of hashes, each hash a single graphite target, value, and time
   def get_graphite (hash, time=Time.now.to_i)
+    raise "Hash was not received" unless hash.is_a? Hash
     data = []
     walk_the_forrest(hash) {|target, value|
       data << { :target => target, :value => value, :time => time }
